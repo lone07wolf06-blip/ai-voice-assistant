9f104a8a80b11a8ee58a06c9d5bc03a93d6d947f
lone07wolf06-blip <lone07wolf06@gmail.com>
2026-02-19 14:51:34 +0530

    Create logger.py with logging configuration

---
+++ logger.py
@@ -0,0 +1,12 @@
+import logging
+
+# Configuring logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='app.log',
+    filemode='a'
+)
+
+logger = logging.getLogger(__name__)
